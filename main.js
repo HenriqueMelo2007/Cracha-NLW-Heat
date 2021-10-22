36333e9510308f903ae0df83335d946bb2accb01
Henrique Melo <86623792+HenriqueMelo2007@users.noreply.github.com>
2021-10-22 17:58:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const LinksSocialMedia = {
+  github: 'HenriqueMelo2007',
+  instagram: 'henriquemelo__14',
+}
+
+
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
